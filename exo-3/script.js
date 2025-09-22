00389ce4d05dbab604fb0d567a71b7e05b924c7d
kharbachdouae <kharbach.douae.solicode@gmail.com>
2025-09-22 09:59:52 +0100

    Add files via upload

---
+++ exo-3/script.js
@@ -0,0 +1,27 @@
+let nombre = parseInt(prompt("Entrez un nombre entre 1 et 7 :"));
+switch (nombre) {
+    case 1:
+        console.log("Lundi");  
+        break;
+    case 2:
+        console.log("Mardi");
+        break;
+    case 3:
+        console.log("Mercredi");
+        break;
+    case 4:
+        console.log("Jeudi");
+        break;
+    case 5:
+        console.log("Vendredi");
+        break;
+    case 6:
+        console.log("Samedi");
+        break;
+    case 7:
+        console.log("Dimanche");
+        break;
+    default:
+        console.log("Nombre invalide");
+        break;
+}
